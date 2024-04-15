bcff71a8fc10a92fc6c24f0b04933514b991e6bd
Wyatt Scott <122546418+WD-Scott@users.noreply.github.com>
2024-04-15 10:10:54 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import requests
 import pandas as pd
-import plotly.graph_objs as go
-import ipywidgets as widgets
 from io import StringIO
+import plotly.graph_objs as go
 
 DATA_URL = "https://raw.githubusercontent.com/WD-Scott/DS5110_Project/main/Data%20Files/interactive.csv"
 
 def fetch_csv(data_url: str) -> pd.DataFrame:
     return pd.read_csv(StringIO(requests.get(data_url).text))
 
-# Function to generate and display the bar chart
 def display_bar_chart(df, player_name, year, outfile):
     if player_name is None or year is None:
         print("Please select a player and a year.")
@@ -39,23 +37,8 @@ def display_bar_chart(df, player_name, year, outfile):
                       legend=dict(x=0, y=1.0, bgcolor='rgba(255, 255, 255, 0)', bordercolor='rgba(255, 255, 255, 0)')
                       )
 
-    # Create interactive widgets for player name and year
-    player_name_widget = widgets.Dropdown(options=df['name'].unique(), description='Player:')
-    year_widget = widgets.Dropdown(options=df['Season'].unique(), description='Year:')
-
-    # Create an output widget for displaying the chart
-    output_widget = widgets.Output()
-
-    def update_chart(change):
-        with output_widget:
-            output_widget.clear_output(wait=True)
-            display_bar_chart(df, player_name=player_name_widget.value, year=year_widget.value, outfile=outfile)
-
-    player_name_widget.observe(update_chart, names='value')
-    year_widget.observe(update_chart, names='value')
-
-    # Display widgets and initial chart
-    display(widgets.VBox([player_name_widget, year_widget, output_widget]))
+    # Save the HTML file
+    fig.write_html(outfile, include_plotlyjs='cdn')
 
 if __name__ == "__main__":
     df = fetch_csv(DATA_URL)
